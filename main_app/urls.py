d166308be840b8b4e0d3f182d4719378b9598291
sornekian <sornekian_sarvornek@gmail.com>
2023-02-03 18:03:49 -0800

    completes part 1 and adds styling

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('swords/', views.swords_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+swords = [
+    {'name': '', 'category': 'Katana', 'age': 'Mid-Late 14th century', 'origin': 'Bizen Province', 'swordsmith': 'Yoshikage', 'blade_material': 'Tamahagane Carbon Steel', 'blade_length': '29.7"'},
+    {'name': '', 'category': 'Tachi', 'age': 'Early-Mid 14th century', 'origin': 'Bizen Province', 'swordsmith': 'Osafune Hidemitsu', 'blade_material': 'Tamahagane Carbon Steel', 'blade_length': '25.6"'},
+    {'name': '', 'category': 'Wakizashi', 'age': 'Late 13th-Early 14th century', 'origin': 'Higo Province', 'swordsmith': 'Enjyu Kunisuke', 'blade_material': 'Tamahagane Carbon Steel', 'blade_length': '13.07"'},
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html') 
+
+def swords_index(request):
+    return render(request, 'swords/index.html', {
+        'swords': swords
+    })
